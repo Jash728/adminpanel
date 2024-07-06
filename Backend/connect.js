7166c1b6231b7115a52317fe356f7c16be86ad49
Jash <shahjash2809@gmail.com>
2024-07-06 21:10:04 +0530

    vercel added

--- Backend/connect.js
+++ Backend/connect.js
@@ -1,10 +1,21 @@
 import mysql from "mysql"
 
+// export const db = mysql.createConnection({
+//     host: "localhost",
+//     user : "root",
+//     password : "jashroot",
+//     database : "adminpanel",
+//     connectionLimit: 10 ,
+//     connectTimeout: 10000,
+//     waitForConnections: true ,
+//     queueLimit: 0 
+// })
+
 export const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user : process.env.DB_USERNAME,
-    password : process.env.DB_PASSWORD,
-    database : process.env.DB_DBNAME,
+    host: "bidtvtg5r4lv0mrhzcvu-mysql.services.clever-cloud.com",
+    user : "ujiadq6a3dtfngxu",
+    password : "1WVmSQKpig3RNBeT0QFp",
+    database : "bidtvtg5r4lv0mrhzcvu",
     connectionLimit: 10 ,
     connectTimeout: 10000,
     waitForConnections: true ,
